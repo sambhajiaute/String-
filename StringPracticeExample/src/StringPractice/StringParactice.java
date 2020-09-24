f4dfd5fda424ccc25742a59f990daeedaf04351a
sam Aute <samaute116@gmail.com>
2020-09-24 14:19:45 +0530

    Added String program

---
+++ StringPracticeExample/src/StringPractice/StringParactice.java
@@ -0,0 +1,17 @@
+package StringPractice;
+
+public class StringParactice {
+	
+	//Practise intern method
+	String s=new String ("Welcome");
+	     String s1=s.intern();
+	    // System.out.println("a");
+	   
+	    
+	     
+	
+	     
+	
+	
+
+}
